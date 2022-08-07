92b8f6661d441789fcb25371199bc189f444cd64
keynene <nonung2@naver.com>
2022-08-07 11:32:12 +0000

    [Python] 재귀함수

--- Python/단계별/test.py
+++ Python/단계별/test.py
@@ -1,49 +1,20 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-pobi = [[97,98], [131,132], [99,102]]
-crong = [[197,198], [211,212], [211,212]]
+def star(n):
+    if n == 3:
+        return ['***', '* *', '***']
+    
+    sq = star(n//3)
+    arr = []
 
-def mult(num):
-    n = len(str(num))
-    res = 0
-    if n == 1 :
-        res = num
-    elif n == 2 :
-        a,b = map(int,str(num))
-        res = a*b
-    elif n == 3 :
-        a,b,c = map(int, str(num))
-        res = a*b*c
-    return res
+    for i in sq:
+        arr.append(i*3)
+    
+    for i in sq:
+        arr.append(i+' '*(n//3)+i)
 
-result = []
+    for i in sq:
+        arr.append(i*3)
 
-for i in range(len(pobi)):
-
-    if pobi[i][0] > 0 and crong[i][0] > 0 and (pobi[i][1]-pobi[i][0]) == 1 and (crong[i][1]-crong[i][0]) == 1:
-        pobiRes = []
-        crongRes = []
-
-        for numP, numC in zip(pobi[i], crong[i]):
-            pobiRes.append(sum(map(int,str(numP))))
-            crongRes.append(sum(map(int,str(numC))))
-            pobiRes.append(mult(numP))
-            crongRes.append(mult(numC))
-            
-        if max(pobiRes) > max(crongRes):
-            result.append(1)
-        elif max(pobiRes) < max(crongRes):
-            result.append(2)
-        elif max(pobiRes) == max(crongRes):
-            result.append(0)
-    else:
-        result.append(-1)
-
-
-print('| pobi       | crong      | result |','| ---------- | ---------- | ------ |',sep="\n")
-for i,j,k in zip(pobi,crong,result):
-    pob = ' '*(10-len(str(i)))
-    cro = ' '*(10-len(str(j)))
-    sp = ' '*(6-len(str(k)))
-    print('| ',i,pob,' |',' ', j, cro, ' |', ' ', k, sp, ' |',sep="")
+    return arr
+print('\n'.join(star(n)))
