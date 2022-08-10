61a121520b6a6ac07021e153b0e6485dad27cd77
keynene <nonung2@naver.com>
2022-08-10 18:02:12 +0900

    module 1 라이브러리, 자바스크립트 선언부 : 모듈화 되어잇어서 필요할 떄 간단하게 쓰기 편함

---
+++ 10. module/module.js
@@ -0,0 +1,4 @@
+function welcome(){
+    return 'Hello world';
+}
+console.log(welcome());
--- 9. object/object.js
+++ 9. object/object.js
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Object</title>
-</head>
-<body>
-<script>
-    var grades = {
-        'list' : {'egoing': 10, 'k8805': 6, 'sorialgi': 80},
-        'show' : function(){
-            for(var name in this.list){
-                document.write(name+' : '+this.list[name]+"<br />");
-            }
-        },
-        'test' : function(){
-           grades.list.egoing = grades.list.egoing+'a';
+
+var grades = {
+    'list' : {'egoing': 10, 'k8805': 6, 'sorialgi': 80},
+    'show' : function(){
+        for(var name in this.list){
+            console.log(name+' : '+this.list[name]+"\n");
         }
+    },
+     'test' : function(){
+         this.list.egoing = this.list.egoing+'a123';
+     }
 
-    };
-    
-    grades.test();
-    grades.show();
-    /*
-    for(key in grades.list){
-        document.write(key+" : "+grades.list[key]+"<br />");
-        
-    }
-    */
+};
+var grades2 = grades;
+grades2.test();
+grades.show();
 
-</script>
-</body>
-</html>
