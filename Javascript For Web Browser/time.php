62740a4306cce09eae83f68fa3e243acd55ba53a
keynene <nonung2@naver.com>
2022-09-05 19:53:10 +0900

    ajax, json, jquery event

---
+++ Javascript For Web Browser/time.php
@@ -0,0 +1,10 @@
+<!-- <?php
+$timezones = ["Asia/Seoul","America/New_York"];
+echo implode(',',$timezones);
+?> -->
+
+
+<?php
+$timezones = ["Asia/Seoul","America/New_York"];
+echo json_encode($timezones);
+?>
