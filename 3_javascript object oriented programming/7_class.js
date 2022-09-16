2563175572e72c7bf93b73549220a8d8362cd135
keynene <nonung2@naver.com>
2022-09-16 14:39:43 +0900

    constructor 안에서의 메소드 실행 방법 prototype 방법 → constructor안에서는 그냥 바로 선언 가능 sum(){  }

--- 3_javascript object oriented programming/7_class.js
+++ 3_javascript object oriented programming/7_class.js
@@ -1,22 +1,26 @@
 // 함수형 객체 생성자로는 선언 시 초기값이 세팅이 되는데 class는 초기값 세팅이 안됨
 // constructor : 객체를 생성되는 과정에서 실행됨
 class Person{
-
-    name;
-    name2 = 10;
-
     constructor(name, first, second){
         this.name = name;
         this.first = first;
         this.second = second;
-        console.log('생성');
+    }
+    // constructor에서의 메소드 실행방법
+    sum(){
+        return 'prototype : '+ (this.first + this.second);
     }
 }
+/*
+// 정석 prototype 메소드 실행 방법
+Person.prototype.sum = function(){
+    return 'prototype : '+ (this.first + this.second);
+}
+*/
 
 var noi = new Person('noi', 10, 20);
 console.log('noi', noi);
-var key = new Person('key', 10, 10);
-console.log('key', key);
+console.log('noi.sum()', noi.sum());
 
 
 // var noi = new Person('noi', 10, 20);
