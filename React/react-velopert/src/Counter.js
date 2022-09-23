22ebbb8dcfd86def39d8222340d9f543a2654992
keynene <nonung2@naver.com>
2022-09-23 20:05:15 +0900

    REACT Coding Apple React copy = [...], onClick, useState   etc

---
+++ React/react-velopert/src/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    render(){
+        return (
+            <div>
+                <h1>카운터</h1>
+            </div>
+        )
+    }
+}
+
+export default Counter;
