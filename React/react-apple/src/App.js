92ebfbb3bd0590269617b20da3d9cdc5429c8064
keynene <nonung2@naver.com>
2022-09-24 16:25:38 +0900

    REACT Coding Apple React component 제작, 장단점

--- React/react-apple/src/App.js
+++ React/react-apple/src/App.js
@@ -23,7 +23,8 @@ function App() {
         }}>Change</button>
         <button onClick={()=>{
           let copy = [...글제목];
-          copy = 글제목.sort();
+          copy.sort();
+          글제목변경(copy);
         }}>가나다순정렬</button>
       <div className="list">
         <h4>{글제목[0]} <span onClick={()=>{
@@ -39,8 +40,23 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      
+      <Modal></Modal>
+      
     </div>
   );
 }
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+
 
 export default App;
