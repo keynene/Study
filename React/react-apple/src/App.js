80a3740b433a1dc538af5f9d1466522e260cbed1
keynene <nonung2@naver.com>
2022-09-24 18:16:31 +0900

    REACT Coding Apple React map 이용하여 반복html 구현

--- React/react-apple/src/App.js
+++ React/react-apple/src/App.js
@@ -8,7 +8,8 @@ function App() {
 
   let post = '강남 우동 맛집'
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
+  let [modal, setModal] = useState(false); // 모달여부 true:열림, false:닫힘
 
 
   return (
@@ -26,7 +27,9 @@ function App() {
           copy.sort();
           글제목변경(copy);
         }}>가나다순정렬</button>
-      <div className="list">
+
+        
+      {/* <div className="list">
         <h4>{글제목[0]} <span onClick={()=>{
           따봉변경(따봉+1)
         }}>👍</span> {따봉} </h4>
@@ -37,11 +40,48 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{
+          if(modal == false){
+            setModal(true);
+          } else {
+            setModal(false);
+          }
+        }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
       
-      <Modal></Modal>
+      
+      { //중괄호{}안에 for 등 반복문 사용불가 → map()
+        // array.map(a,b) : a에 array[0], array[1], array[2] 저장됨, b는 반복문 돌 떄마다 0부터 1씩 증가함
+        // 글제목.map(a) : a = 남자코트추천 등등, b=0~2
+        글제목.map(function(data, i){ //글제목 array길이만큼 반복
+          return (
+            <div className="list" key={i}> 
+            {/* 반복된 html안에는 key값이 있어야 함, key값을 지정하면 warning 사라짐 */}
+              <h4 onClick={()=>{
+                if(modal == false){
+                  setModal(true);
+                } else {
+                  setModal(false);
+                }
+              }}>{ 글제목[i] } <span onClick={()=>{
+                let copy = [...따봉];
+                copy[i] = copy[i]+1;
+                따봉변경(copy); 
+              }}>👍</span> {따봉[i]} </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+
+
+      { //컴포넌트 중간에 "중괄호{}" 입력 시 안에 자바스크립트 코드 기입 가능
+        //조건식 작성 시 삼항연산자 이용
+        // 조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드
+        modal == true ? <Modal /> : null
+      }
       
     </div>
   );
