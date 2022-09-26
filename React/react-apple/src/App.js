d6508ae0c355e7f15b34596bd46dadc9067d6234
keynene <nonung2@naver.com>
2022-09-26 16:11:36 +0900

    REACT Coding Apple React 모달창에 글제목 연동 title state를 만들고 Modal으로 title값을 props로 전송하여 연동 글제목 클릭 시 => setTitle(i) Modal함수 => {props.글제목[props.title]}

--- React/react-apple/src/App.js
+++ React/react-apple/src/App.js
@@ -10,14 +10,14 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false); // 모달여부 true:열림, false:닫힘
-
+  let [title, setTitle] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
           let copy = [...글제목];
           copy[0] = '여자코트 추천';
           글제목변경(copy);
@@ -26,7 +26,7 @@ function App() {
           let copy = [...글제목];
           copy.sort();
           글제목변경(copy);
-        }}>가나다순정렬</button>
+        }}>가나다순정렬</button> */}
 
         
       {/* <div className="list">
@@ -58,13 +58,7 @@ function App() {
           return (
             <div className="list" key={i}> 
             {/* 반복된 html안에는 key값이 있어야 함, key값을 지정하면 warning 사라짐 */}
-              <h4 onClick={()=>{
-                if(modal == false){
-                  setModal(true);
-                } else {
-                  setModal(false);
-                }
-              }}>{ 글제목[i] } <span onClick={()=>{
+              <h4 onClick={()=>{setModal(true); setTitle(i);}}>{ 글제목[i] } <span onClick={()=>{
                 let copy = [...따봉];
                 copy[i] = copy[i]+1;
                 따봉변경(copy); 
@@ -80,19 +74,27 @@ function App() {
       { //컴포넌트 중간에 "중괄호{}" 입력 시 안에 자바스크립트 코드 기입 가능
         //조건식 작성 시 삼항연산자 이용
         // 조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드
-        modal == true ? <Modal /> : null
+        modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}/> : null
+        //부모함수 → 자식함수 state 넘길 때 <Modal 작명={state}/> 하면됨
       }
       
     </div>
   );
 }
 
-function Modal(){
+
+
+function Modal(props){
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>제목 : {props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{
+        let copy = [...props.글제목]
+        copy[0] = '여자코트 추천'
+        props.글제목변경(copy)
+      }}>글수정</button>
     </div>
   )
 }
