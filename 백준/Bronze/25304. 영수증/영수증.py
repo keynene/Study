48397d77c03249466b5dc8dc37ab651bb2e698d5
keynene <109068358+keynene@users.noreply.github.com>
2022-09-26 19:57:16 +0900

    [Bronze V] Title: 영수증, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -2,6 +2,6 @@ import sys
 X = int(input())
 plus = 0
 for i in range(int(input())):
-    a,b = map(int,input().split())
+    a,b = map(int,sys.stdin.readline().split())
     plus = plus+a*b
 print('Yes' if X==plus else 'No');
