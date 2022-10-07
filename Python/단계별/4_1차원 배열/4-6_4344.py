3657dded13e1fc9183c4984610785acce97ae54b
keynene <nonung2@naver.com>
2022-10-07 16:33:44 +0900

    [Python] 10/7 문제풀이 4-1차원배열 end

--- Python/단계별/4_1차원 배열/4-6_4344.py
+++ Python/단계별/4_1차원 배열/4-6_4344.py
@@ -1,17 +1,15 @@
 import sys
+
 C = int(input())
 N = 0;
 
 for i in range(C):
     score = list(map(int, sys.stdin.readline().split()))
+    avg = (sum(score)-score[0])/score[0];
     cnt = 0;
-    for j in score:
-        if j == 0 :
-            continue;
-        else:
-            avg = (sum(score)-score[0])/score[0]
-            if score[j] > avg :
-                cnt += 1
-            else:
-                continue;
-    print(cnt/score[0])
+    for num in score[1:]:
+        if num > avg :
+            cnt += 1;
+    rate = (cnt/score[0])*100
+    print(f'{rate:.3f}%')
+   
