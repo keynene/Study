3b736919eb24d2de0c5ad7ff7b1c07b9f38688c7
keynene <109068358+keynene@users.noreply.github.com>
2022-10-18 19:07:56 +0900

    [Silver III] Title: 소수 구하기, Time: 948 ms, Memory: 117128 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,14 @@
 import sys
-M, N = map(int, sys.stdin.readline().split())
+m,n = map(int, sys.stdin.readline().split())
 
-def check_prime(num):
+def sosu(num):
     if num == 1: return False
     else:
-        for i in range(2, int(num**0.5) + 1):
+        for i in range(2, int(num**0.5)+1):
             if num % i == 0:
                 return False
         return True
 
-for i in range(M, N + 1):
-    if check_prime(i):
+for i in range(m, n+1):
+    if sosu(i):
         print(i)
