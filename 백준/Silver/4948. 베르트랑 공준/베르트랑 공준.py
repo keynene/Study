c8149f0a00b11e1f2fba669b33457ce46a44d0c8
keynene <109068358+keynene@users.noreply.github.com>
2022-10-19 16:55:34 +0900

    [Silver III] Title: 베르트랑 공준, Time: 336 ms, Memory: 115732 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,23 +1,25 @@
-import sys
-input = sys.stdin.readline
+num = []
 
-def gj(n):
-    err = 0
-    for j in range(2,int((n**0.5))+1):
-        if n%j == 0:
-            return False
-    if err == 0:
-        return True
+for i in range(2, 246913):
+    cnt = 0
 
-cnt = 0
+    for p in range(2, int(i**0.5)+1):
+        if i % p == 0:
+            cnt += 1
+            break
+
+    if cnt == 0:
+        num.append(i)
 
 while True:
-    n = int(input().rstrip())
-    cnt = 0
-    if n != 0:
-        for i in range(n+1,(2*n)+1):
-            if gj(i):
-                cnt += 1
-        print(cnt)
-    else:
+    n = int(input())
+    res = 0
+
+    if n == 0:
         break
+
+    for i in num:
+        if n < i <= 2*n:
+            res += 1
+
+    print(res)
