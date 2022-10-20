d94cf2943f14496ffcb48b48371e400388a230fb
keynene <109068358+keynene@users.noreply.github.com>
2022-10-21 01:38:58 +0900

    [Silver I] Title: 회의실 배정, Time: 428 ms, Memory: 137540 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -3,12 +3,10 @@ input = sys.stdin.readline
 
 n = int(input())
 conf = sorted([list(map(int,input().split())) for _ in range(n)], key=lambda x:(x[1],x[0]))
-cnt = 0
 max_conf = [0]
 
 
 for time in conf:
     if time[0] >= max_conf[-1]:
-        cnt += 1
         max_conf.append(time[1])
-print(cnt)
+print(len(max_conf)-1)
