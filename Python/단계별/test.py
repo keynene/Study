cb93fa299dfc51e62efd0fb7796547870bfbedd6
keynene <nonung2@naver.com>
2022-10-20 23:11:00 +0900

    [Python] 10/20 문제풀이 이전문제 복습

--- Python/단계별/test.py
+++ Python/단계별/test.py
@@ -1,13 +1,56 @@
 import sys
 input = sys.stdin.readline
 
-m,n = map(int,input().split())
+t = int(input().rstrip())
 
-for i in range(m,n+1):
-    err = 0
-    for j in range(2, int(i**0.5)+1):
-        if i > 1:
-            if i%j == 0:
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+for _ in range(t):
+    n = int(input().rstrip())
+    a,b = n//2, n//2
+    while a > 0:
+        if prime(a) and prime(b):
+            print(a, b)
+            break
+        else:
+            a -= 1
+            b += 1
+
+
+
+
+
+### 시간초과 (맞왜틀) ### 
+import sys
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+
+def prime(n):
+    res = []
+    res_prime = ''
+    for num in range(n):
+        err = 0
+        for i in range(2,int(num**0.5)+1):
+            if num%i == 0:
                 err += 1
-    if err == 0:
-        print(i)
+        if err == 0:
+            res.append(num)
+    cnt = max(res)-min(res)
+    for a in res:
+        for b in res:
+            if a+b == n:
+                if abs(a-b) < cnt:
+                    cnt = abs(a-b)
+                    res_prime = str(a)+' '+str(b)
+                else:
+                    pass
+    return res_prime
+
+for _ in range(t):
+    n = int(input().rstrip())
+    print(prime(n))
