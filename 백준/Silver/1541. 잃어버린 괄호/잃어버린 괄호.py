03225c4ee2a327c3abdb919d093e9ca90ce2d7dd
keynene <109068358+keynene@users.noreply.github.com>
2022-10-21 22:49:45 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+s = str(input().rstrip())
+cnt = 0
+save = ''
+num = []
+f = []
+
+for i in range(len(s)):
+    save += s[i]
+    if s[i]=='+' or s[i]=='-':
+        num.append(int(save[0:-1]))
+        f.append(s[i])
+        save=''
+    if i==len(s)-1:
+        num.append(int(save))
+
+while len(f)!=0:
+    for i in range(len(f)):
+        if f[i]=='+':
+            num[i] = num[i]+num[i+1]
+            del num[i+1]
+            del f[i]
+            break
+        
+        elif f[i]=='-' and '+' not in f:  
+            num[i+1] *= -1
+            num[i] = num[i]+num[i+1]
+            del num[i+1]
+            del f[i]
+            break
+print(*num)
