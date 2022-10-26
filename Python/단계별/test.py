aa2af2a4f9357e8f7869c8f90bf4494064d31396
keynene <nonung2@naver.com>
2022-10-26 23:36:44 +0900

    [Python] 10/26 문제풀이 백트래킹,dfs

--- Python/단계별/test.py
+++ Python/단계별/test.py
@@ -1,25 +1,49 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-root = [list(map(int,input().rstrip())) for _ in range(n)]
+pobi = [[97,98], [131,132], [99,102]]
+crong = [[197,198], [211,212], [211,212]]
+
+def mult(num):
+    n = len(str(num))
+    res = 0
+    if n == 1 :
+        res = num
+    elif n == 2 :
+        a,b = map(int,str(num))
+        res = a*b
+    elif n == 3 :
+        a,b,c = map(int, str(num))
+        res = a*b*c
+    return res
+
+result = []
+
+for i in range(len(pobi)):
+
+    if pobi[i][0] > 0 and crong[i][0] > 0 and (pobi[i][1]-pobi[i][0]) == 1 and (crong[i][1]-crong[i][0]) == 1:
+        pobiRes = []
+        crongRes = []
+
+        for numP, numC in zip(pobi[i], crong[i]):
+            pobiRes.append(sum(map(int,str(numP))))
+            crongRes.append(sum(map(int,str(numC))))
+            pobiRes.append(mult(numP))
+            crongRes.append(mult(numC))
+            
+        if max(pobiRes) > max(crongRes):
+            result.append(1)
+        elif max(pobiRes) < max(crongRes):
+            result.append(2)
+        elif max(pobiRes) == max(crongRes):
+            result.append(0)
+    else:
+        result.append(-1)
 
 
-def bfs(x,y,cnt):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return cnt
-    if root[x][y] == 1:
-        cnt += 1
-        root[x][y] = 0
-        bfs(x-1, y,cnt)
-        bfs(x, y-1,cnt)
-        bfs(x+1, y,cnt)
-        bfs(x, y+1,cnt)
-
-
-    if x == n-1 and y == m-1:
-        return print(cnt)
-    return cnt
-
-for i in range(n):
-    for j in range()
+print('| pobi       | crong      | result |','| ---------- | ---------- | ------ |',sep="\n")
+for i,j,k in zip(pobi,crong,result):
+    pob = ' '*(10-len(str(i)))
+    cro = ' '*(10-len(str(j)))
+    sp = ' '*(6-len(str(k)))
+    print('| ',i,pob,' |',' ', j, cro, ' |', ' ', k, sp, ' |',sep="")
