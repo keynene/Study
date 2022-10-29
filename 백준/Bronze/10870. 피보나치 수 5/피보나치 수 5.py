6b65a719088951c2986d5f5d014a656e38959c05
keynene <109068358+keynene@users.noreply.github.com>
2022-10-29 18:21:34 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,8 +1,8 @@
-import sys
 def fibo(n):
     if n <= 1:
         return n
-    return fibo(n-1) + fibo(n-2)
-            
-tc = int(sys.stdin.readline())
-print(fibo(tc))
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+n = int(input())
+print(fibo(n))
