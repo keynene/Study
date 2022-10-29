eed57933e31e37fd705db1148bc7a8c5aa6326f8
keynene <nonung2@naver.com>
2022-10-29 23:37:24 +0900

    Python 백트래킹, 재귀

--- Python/단계별/10_재귀/10-3_25501.py
+++ Python/단계별/10_재귀/10-3_25501.py
@@ -1,17 +1,36 @@
-import sys
-
-
-def recursion(s, l, r):
-    global cnt
-    cnt += 1;
-
-    if l >= r: return 1
-    elif s[l] != s[r]: return 0
-    else: return recursion(s, l+1, r-1)
+def recursion(s, l, r, cnt):
+    cnt += 1
+    if l >= r: return 1, cnt
+    elif s[l] != s[r]: return 0, cnt
+    else: return recursion(s, l+1, r-1, cnt)
 
 def isPalindrome(s):
-    return recursion(s, 0, len(s)-1)
+    cnt = 0 
+    return recursion(s, 0, len(s)-1, cnt)
 
-for _ in range(int(input())):
-    cnt = 0
-    print(isPalindrome(sys.stdin.readline().strip()),cnt)
+
+
+import sys
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+s = [print(*isPalindrome(input().rstrip())) for _ in range(t)]
+
+###다른풀이 (global)###
+# import sys
+
+
+# def recursion(s, l, r):
+#     global cnt
+#     cnt += 1;
+
+#     if l >= r: return 1
+#     elif s[l] != s[r]: return 0
+#     else: return recursion(s, l+1, r-1)
+
+# def isPalindrome(s):
+#     return recursion(s, 0, len(s)-1)
+
+# for _ in range(int(input())):
+#     cnt = 0
+#     print(isPalindrome(sys.stdin.readline().strip()),cnt)
--- Python/단계별/16_백트래킹/16-7_14888(false).py
+++ Python/단계별/16_백트래킹/16-7_14888(false).py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-num = list(map(int, input().split()))
-f = list(map(int, input().split()))
+n = 3
+a = [3, 4, 5]
+f = [1, 0, 1, 0]
 
 maxN = -1e9
 minN = 1e9
 
-def dfs(idx, total, pl, mi, mul, div):
+def dfs(idx, total, sum, sub, mul, div):
     global maxN, minN
+
     if idx == n:
         maxN = max(total, maxN)
         minN = min(total, minN)
         return
-
-    if pl:
-        dfs(idx+1, total+num[idx], pl-1, mi, mul,div)
-    if mi:
-        dfs(idx+1, total-num[idx], pl, mi-1, mul,div)
-    if mul:
-        dfs(idx+1, total*num[idx], pl, mi, mul-1,div)
-    if div:
+    
+    if sum :
+        dfs(idx+1, total+a[idx], sum-1,sub, mul, div)
+    if sub :
+        dfs(idx+1, total-a[idx], sum, sub-1, mul, div)
+    if mul :
+        dfs(idx+1, total*a[idx], sum, sub, mul-1, div)
+    if div :
         if total < 0:
             total *= -1
-        dfs(idx+1, total//num[idx], pl, mi, mul,div-1)
+        dfs(idx+1, total//a[idx], sum, sub, mul, div-1)
 
-dfs(1, num[0], f[0], f[1], f[2], f[3])
+dfs(1,a[0],f[0],f[1],f[2],f[3])
 print(maxN)
 print(minN)
 
@@ -38,47 +39,76 @@ print(minN)
 
 
 
-#이해안가는 알고리즘 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import sys
 # input = sys.stdin.readline
 
 # n = int(input().rstrip())
-# number = list(map(int, input().split()))
+# num = list(map(int, input().split()))
 # f = list(map(int, input().split()))
-# minN = 1e9
+
 # maxN = -1e9
-# ans = number[0]
+# minN = 1e9
 
-# def dfs(idx):
-#     global ans
-#     global minN, maxN
-
+# def dfs(idx, total, pl, mi, mul, div):
+#     global maxN, minN
 #     if idx == n:
-#         if ans > maxN:
-#             maxN = ans
-#         elif ans < minN:
-#             minN = ans
+#         maxN = max(total, maxN)
+#         minN = min(total, minN)
 #         return
-    
-#     for i in range(4):
-#         tmp = ans
-#         if f[i] > 0:
-#             if i == 0:
-#                 ans += number[idx]
-#             elif i == 1:
-#                 ans -= number[idx]
-#             elif i == 2:
-#                 ans *= number[idx]
-#             else:
-#                 if ans < 0:
-#                     ans *= -1
-#                 ans = ans//number[idx]
-            
-#             f[i] -= 1
-#             dfs(idx+1)
-#             ans = tmp
-#             f[i] += 1
-# dfs(1)
+
+#     if pl:
+#         dfs(idx+1, total+num[idx], pl-1, mi, mul,div)
+#     if mi:
+#         dfs(idx+1, total-num[idx], pl, mi-1, mul,div)
+#     if mul:
+#         dfs(idx+1, total*num[idx], pl, mi, mul-1,div)
+#     if div:
+#         if total < 0:
+#             total *= -1
+#         dfs(idx+1, total//num[idx], pl, mi, mul,div-1)
+
+# dfs(1, num[0], f[0], f[1], f[2], f[3])
 # print(maxN)
 # print(minN)
 
@@ -89,71 +119,123 @@ print(minN)
 
 
 
-# 내알고리즘...
-# import sys
-# input = sys.stdin.readline
 
-# n = int(input().rstrip())
-# a = list(map(int, input().split()))
-# plus, minus, mul, div = map(int, input().split())
+# #이해안가는 알고리즘 1
+# # import sys
+# # input = sys.stdin.readline
 
-# max_list = [minus, div, plus, mul] #1,1,2,1
-# min_list = [plus, div, minus, mul]
+# # n = int(input().rstrip())
+# # number = list(map(int, input().split()))
+# # f = list(map(int, input().split()))
+# # minN = 1e9
+# # maxN = -1e9
+# # ans = number[0]
 
-# res_max = []
-# res_min = []
+# # def dfs(idx):
+# #     global ans
+# #     global minN, maxN
 
-# for i in a:
-#     res_max.append(i)
-#     res_min.append(i)
-
-
-# def a_max(num):
-#     while len(num) > 1:
-#         for i in range(len(num)-1):
-#             if max_list[0] != 0:
-#                 num[i] = num[i] - num[i+1]
-#                 max_list[0] -= 1
-
-#             elif max_list[1] != 0:
-#                 if num[i] < 0:
-#                     num[i] *= -1
-#                 num[i] = num[i] // num[i+1]
-#                 max_list[1] -= 1
-
-#             elif max_list[2] != 0:
-#                 num[i] = num[i] + num[i+1]
-#                 max_list[2] -= 1
+# #     if idx == n:
+# #         if ans > maxN:
+# #             maxN = ans
+# #         elif ans < minN:
+# #             minN = ans
+# #         return
+    
+# #     for i in range(4):
+# #         tmp = ans
+# #         if f[i] > 0:
+# #             if i == 0:
+# #                 ans += number[idx]
+# #             elif i == 1:
+# #                 ans -= number[idx]
+# #             elif i == 2:
+# #                 ans *= number[idx]
+# #             else:
+# #                 if ans < 0:
+# #                     ans *= -1
+# #                 ans = ans//number[idx]
             
-#             elif max_list[3] != 0:
-#                 num[i] = num[i] * num[i+1]
-#                 max_list[3] -= 1
-#             del num[i+1]
-#             break
-#     return num
-# print(a_max(res_max))
+# #             f[i] -= 1
+# #             dfs(idx+1)
+# #             ans = tmp
+# #             f[i] += 1
+# # dfs(1)
+# # print(maxN)
+# # print(minN)
 
-# def a_min(num):
-#     while len(num) > 1:
-#         for i in range(len(num)-1):
-#             if min_list[0] != 0:
-#                 num[i] = num[i] + num[i+1]
-#                 min_list[0] -= 1
 
-#             elif min_list[1] != 0:
-#                 if num[i] < 0:
-#                     num[i] *= -1
-#                 num[i] = num[i] // num[i+1]
-#                 min_list[1] -= 1
+
+
+
+
+
+
+# # 내알고리즘...
+# # import sys
+# # input = sys.stdin.readline
+
+# # n = int(input().rstrip())
+# # a = list(map(int, input().split()))
+# # plus, minus, mul, div = map(int, input().split())
+
+# # max_list = [minus, div, plus, mul] #1,1,2,1
+# # min_list = [plus, div, minus, mul]
+
+# # res_max = []
+# # res_min = []
+
+# # for i in a:
+# #     res_max.append(i)
+# #     res_min.append(i)
+
+
+# # def a_max(num):
+# #     while len(num) > 1:
+# #         for i in range(len(num)-1):
+# #             if max_list[0] != 0:
+# #                 num[i] = num[i] - num[i+1]
+# #                 max_list[0] -= 1
+
+# #             elif max_list[1] != 0:
+# #                 if num[i] < 0:
+# #                     num[i] *= -1
+# #                 num[i] = num[i] // num[i+1]
+# #                 max_list[1] -= 1
+
+# #             elif max_list[2] != 0:
+# #                 num[i] = num[i] + num[i+1]
+# #                 max_list[2] -= 1
             
-#             elif min_list[2] != 0:
-#                 num[i] = num[i] - num[i+1]
-#                 min_list[2] -= 1
+# #             elif max_list[3] != 0:
+# #                 num[i] = num[i] * num[i+1]
+# #                 max_list[3] -= 1
+# #             del num[i+1]
+# #             break
+# #     return num
+# # print(a_max(res_max))
 
-#             elif min_list[3] != 0:
-#                 num[i] = num[i] * num[i+1]
-#                 min_list[3] -= 1
-#             del num[i+1]
-#             break
-#     return num
-# print(a_min(res_min))
+# # def a_min(num):
+# #     while len(num) > 1:
+# #         for i in range(len(num)-1):
+# #             if min_list[0] != 0:
+# #                 num[i] = num[i] + num[i+1]
+# #                 min_list[0] -= 1
+
+# #             elif min_list[1] != 0:
+# #                 if num[i] < 0:
+# #                     num[i] *= -1
+# #                 num[i] = num[i] // num[i+1]
+# #                 min_list[1] -= 1
+            
+# #             elif min_list[2] != 0:
+# #                 num[i] = num[i] - num[i+1]
+# #                 min_list[2] -= 1
+
+# #             elif min_list[3] != 0:
+# #                 num[i] = num[i] * num[i+1]
+# #                 min_list[3] -= 1
+# #             del num[i+1]
+# #             break
+# #     return num
+# # print(a_min(res_min))
