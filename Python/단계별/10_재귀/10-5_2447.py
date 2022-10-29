d60b60c7fd5f04f3615a96846c1952d854e986e3
keynene <nonung2@naver.com>
2022-10-29 15:57:23 +0900

    Revert "[Python]"
    This reverts commit 92b8f6661d441789fcb25371199bc189f444cd64.

--- Python/단계별/10_재귀/10-5_2447.py
+++ Python/단계별/10_재귀/10-5_2447.py
@@ -1,21 +1,49 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-def star(n):
-    if n == 3:
-        return ['***','* *','***']
+pobi = [[97,98], [131,132], [99,102]]
+crong = [[197,198], [211,212], [211,212]]
 
-    arr = star(n//3)
-    stars = []
+def mult(num):
+    n = len(str(num))
+    res = 0
+    if n == 1 :
+        res = num
+    elif n == 2 :
+        a,b = map(int,str(num))
+        res = a*b
+    elif n == 3 :
+        a,b,c = map(int, str(num))
+        res = a*b*c
+    return res
 
-    for i in arr:
-        stars.append(i*3)
+result = []
 
-    for i in arr:
-        stars.append(i+' '*(n//3)+i)
+for i in range(len(pobi)):
 
-    for i in arr:
-        stars.append(i*3)
+    if pobi[i][0] > 0 and crong[i][0] > 0 and (pobi[i][1]-pobi[i][0]) == 1 and (crong[i][1]-crong[i][0]) == 1:
+        pobiRes = []
+        crongRes = []
 
-    return stars
+        for numP, numC in zip(pobi[i], crong[i]):
+            pobiRes.append(sum(map(int,str(numP))))
+            crongRes.append(sum(map(int,str(numC))))
+            pobiRes.append(mult(numP))
+            crongRes.append(mult(numC))
+            
+        if max(pobiRes) > max(crongRes):
+            result.append(1)
+        elif max(pobiRes) < max(crongRes):
+            result.append(2)
+        elif max(pobiRes) == max(crongRes):
+            result.append(0)
+    else:
+        result.append(-1)
 
-print('\n'.join(star(n)))
+
+print('| pobi       | crong      | result |','| ---------- | ---------- | ------ |',sep="\n")
+for i,j,k in zip(pobi,crong,result):
+    pob = ' '*(10-len(str(i)))
+    cro = ' '*(10-len(str(j)))
+    sp = ' '*(6-len(str(k)))
+    print('| ',i,pob,' |',' ', j, cro, ' |', ' ', k, sp, ' |',sep="")
