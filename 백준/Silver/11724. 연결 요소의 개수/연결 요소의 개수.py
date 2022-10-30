a0d4cbc206a0e90b1433c7a8687f09e80824c93d
keynene <109068358+keynene@users.noreply.github.com>
2022-10-31 00:01:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 492 ms, Memory: 204928 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+node = [list(map(int, input().split())) for _ in range(m)]
+amap = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+
+for u,v in node:
+    amap[u].append(v)
+    amap[v].append(u)
+cnt = 0
+
+def dfs(v):
+    global cnt
+    for i in amap[v]:
+        if not visited[i]:
+            visited[i] = 1
+            dfs(i)
+
+for i in range(1, n+1):
+    if not visited[i]:
+        visited[i] = 1
+        dfs(i)
+        cnt += 1
+
+print(cnt)
