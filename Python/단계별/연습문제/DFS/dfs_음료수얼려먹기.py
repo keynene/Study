46e2ecf873cc211a844dd53a37443d9727a17e94
keynene <nonung2@naver.com>
2022-11-03 22:21:02 +0900

    Python DFS dx,dy 복습

--- Python/단계별/연습문제/DFS/dfs_음료수얼려먹기.py
+++ Python/단계별/연습문제/DFS/dfs_음료수얼려먹기.py
@@ -1,27 +1,54 @@
-#음료수 얼려 먹기
-
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 n,m = map(int, input().split())
-ice = [list(map(int,input().rstrip())) for _ in range(n)]
-res = 0
+ice = [list(map(int, input().rstrip())) for _ in range(n)]
+cnt = 0
 
 def dfs(x,y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    if ice[x][y] == 0:
-        ice[x][y] = 1
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        return True
-    return False
+    ice[y][x] = 1
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0 <= nx < m and 0 <= ny < n and ice[ny][nx] == 0:
+            dfs(nx,ny)
+            
 
-for i in range(n):
-    for j in range(m):
-        if dfs(i,j) == True:
-            res += 1
+for y in range(n):
+    for x in range(m):
+        if not ice[y][x]:
+            dfs(x,y)
+            cnt += 1
+print(cnt)
 
-print(res)
+
+# #음료수 얼려 먹기
+
+# import sys
+# input = sys.stdin.readline
+
+# n,m = map(int, input().split())
+# ice = [list(map(int,input().rstrip())) for _ in range(n)]
+# res = 0
+
+# def dfs(x,y):
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+#     if ice[x][y] == 0:
+#         ice[x][y] = 1
+#         dfs(x-1, y)
+#         dfs(x, y-1)
+#         dfs(x+1, y)
+#         dfs(x, y+1)
+#         return True
+#     return False
+
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i,j) == True:
+#             res += 1
+
+# print(res)
