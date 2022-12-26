61c39ab374867fe3e2e2bb8668cad8a22e219e90
keynene <nonung2@naver.com>
2022-12-26 19:38:40 +0900

    [Python] BFS 알고리즘

--- Python/Baekjoon/연습문제/BFS/bfs_2178.py
+++ Python/Baekjoon/연습문제/BFS/bfs_2178.py
@@ -1,29 +1,63 @@
-#21:33 #21:55
+# #21:33 #21:55
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# n,m = map(int, input().split())
+# graph = [list(map(int,input().rstrip())) for _ in range(n)]
+
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+
+# def bfs(x,y):
+#     queue = deque()
+#     queue.append((x,y))
+
+#     while queue:
+#         x,y = queue.popleft()
+
+#         for i in range(4):
+#             nx = x+dx[i]
+#             ny = y+dy[i]
+
+#             if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1:
+#                 graph[ny][nx] = graph[y][x] + 1
+#                 queue.append((nx,ny))
+
+#     return graph[n-1][m-1]
+
+# print(bfs(0,0))
+
+
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-graph = [list(map(int,input().rstrip())) for _ in range(n)]
+N,M = map(int, input().split())
+amap = [list(map(int,input().rstrip())) for _ in range(N)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def bfs(x,y):
     queue = deque()
-    queue.append((x,y))
+    queue.append((x,y,1))
 
     while queue:
-        x,y = queue.popleft()
-
+        x,y,dep = queue.popleft()
+        
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
 
-            if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1:
-                graph[ny][nx] = graph[y][x] + 1
-                queue.append((nx,ny))
+            if 0<=nx<M and 0<=ny<N and amap[ny][nx]==1:
+                amap[ny][nx] = amap[y][x]+1
+                queue.append((nx,ny,dep+1))
 
-    return graph[n-1][m-1]
+for y in range(N):
+    for x in range(M):
+        if amap[y][x] == 1:
+            bfs(x,y)
 
-print(bfs(0,0))
+print(amap[N-1][M-1])
+
