259ccce62ee32ed7ad2764d70367290b0ba50012
keynene <nonung2@naver.com>
2023-04-15 12:44:21 +0000

    [Python] DFS/BFS

---
+++ Python/SWEA/1210_Ladder1.py
@@ -0,0 +1,49 @@
+from collections import deque
+from copy import deepcopy
+
+dx = [-1,1]
+
+def bfs(x,y):
+  global res, ans
+  queue = deque()
+  queue.append((x,y))
+  answer = x
+
+  while queue:
+    x,y = queue.popleft()
+    isChange = False
+
+    if y == 99:
+      if ladder[y][x] == 2:
+        ans = answer
+      return
+
+    for i in range(2):
+      cnt = 0
+      nx = x+dx[i]
+
+      if 0<=nx<100 and ladder[y][nx] == 1:
+        while 0<=nx<100 and ladder[y][nx] > 0 :
+          nx += dx[i]
+          cnt += 1
+        nx -= dx[i]
+        queue.append((nx,y+1))
+        isChange = True
+
+    if not isChange:  
+      queue.append((x,y+1))
+
+
+for T in range(1,11):
+  tc = int(input())
+  ladder = [list(map(int,input().split())) for _ in range(100)]
+
+  res = 0
+  ans = 0
+
+  for y in range(1):
+    for x in range(100):
+      if ladder[y][x] == 1:
+        bfs(x,y)
+  
+  print('#{} {}'.format(T,ans))
