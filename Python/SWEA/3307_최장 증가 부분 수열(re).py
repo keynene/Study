eb93429a38044ef3d9d1d818b86d6a44cfa49654
keynene <nonung2@naver.com>
2023-06-11 12:50:12 +0000

    [Python] SWEA :: 3307

---
+++ Python/SWEA/3307_최장 증가 부분 수열(re).py
@@ -0,0 +1,13 @@
+for T in range(int(input())):
+  N = int(input())
+  arr = [0]+list(map(int, input().split()))
+  dp = [0]*(N+1)
+
+  for i in range(1,N+1):
+    mx = 0
+    for j in range(i):
+      if arr[i] > arr[j]:
+        mx = max(mx,dp[j])
+    dp[i] = mx+1
+
+  print(f'#{T+1} {max(dp)}')
